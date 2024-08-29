135b25a9c843b4ea4101819a80221f75891a1cbc
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-08-29 14:45:26 +0600

    chain

---
+++ chain.js
@@ -0,0 +1,39 @@
+// data access
+const data = [{id:12, salary:323232}];
+
+console.log(data[0].salary); //obj inside array thats why [0]
+
+
+const products = {
+    count:23,
+    data:[
+        {id:1, name: 'apple', price:23000},
+        {id:2, name:'lenevo', price:2322}
+    ]
+}
+
+console.log(products.data[1].price);
+
+const user = {
+    name: 'ashik',
+    address: {
+        street:{
+            first:'23 street0',
+            second: 'bangla bazar',
+            third:'hoga  mara'
+        }
+    },
+    city:'dhaka'
+
+}
+const user2 = {
+    name: 'ashik',
+    address: {
+        city:'chitta',
+        country: 'bd'
+    },
+    city:'dhaka'
+
+}
+console.log(user.address.street.second);
+console.log(user2.address.street?.second);
